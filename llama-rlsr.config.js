3b9e349387791f55e3b6e1c694b9024af41838d2
Jonathan Law <jonlaw10170@gmail.com>
2017-08-31 00:55:02 +0100

    llama-rlsr stuff

---
+++ llama-rlsr.config.js
@@ -0,0 +1,34 @@
+const llamaRlsrKeepAChangelog = require('llama-rlsr-keep-a-changelog');
+const llamaRlsrNpm = require('llama-rlsr-npm');
+const simpleGit = require('simple-git');
+
+module.exports = {
+  preRelease: [
+    LlamaRlsrNpm.updateVersion(),
+    LlamaRlsrKeepAChangelog.updateChangelog({
+      placeholder: '- Nothing yet'
+    }),
+    LlamaRlsrKeepAChangelog.updateDiff({
+      urlGenerator: (oldVersion, newVersion) => {
+        return `https://github.com/HopefulLlama/llama-rlsr/compare/${oldVersion}...${newVersion}`;
+      },
+      latest: 'HEAD',
+      tag: {
+        prefix: 'v'
+      }
+    })
+  ],
+  release: [(versionMetadata, done) => {
+    simpleGit.add(['package.json', 'CHANGELOG.md'], () => {
+      simpleGit.commit(`Update to version ${versionMetadata.newVersion}`, () => {
+        simpleGit.addTag(`v${versionMetadata.newVersion}`, () => {
+          simpleGit.push('origin', 'master', () => {
+            simpleGit.pushTags('origin', () => {
+              done();
+            });
+          });
+        });
+      });
+    });
+  }]
+};

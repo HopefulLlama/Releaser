89d533fffe210e32105df83ccfaab99424153f1a
Jonathan Law <jonlaw10170@gmail.com>
2017-09-01 22:18:42 +0100

    Add GitHub stuff to release process

--- llama-rlsr.config.js
+++ llama-rlsr.config.js
@@ -1,6 +1,13 @@
 const LlamaRlsrKeepAChangelog = require('llama-rlsr-keep-a-changelog');
 const LlamaRlsrNpm = require('llama-rlsr-npm');
 const simpleGit = require('simple-git')(process.cwd());
+const GitHubApi = require('github-api');
+
+const gitHubCredentials = require('./credentials/github.json');
+
+if(!gitHubCredentials) {
+  throw new Error('GitHub credentials not found.');
+}
 
 module.exports = {
   preRelease: [
@@ -18,17 +25,27 @@ module.exports = {
       }
     })
   ],
-  release: [(versionMetadata, done) => {
-    simpleGit.add(['package.json', 'CHANGELOG.md'], () => {
-      simpleGit.commit(`Update to version ${versionMetadata.newVersion}`, () => {
-        simpleGit.addTag(`v${versionMetadata.newVersion}`, () => {
-          simpleGit.push('origin', 'master', () => {
-            simpleGit.pushTags('origin', () => {
-              done();
+  release: [
+    (versionMetadata, done) => {
+      simpleGit.add(['package.json', 'CHANGELOG.md', 'llama-rlsr.metadata.json'], () => {
+        simpleGit.commit(`Update to version ${versionMetadata.newVersion}`, () => {
+          simpleGit.addTag(`v${versionMetadata.newVersion}`, () => {
+            simpleGit.push('origin', 'master', () => {
+              simpleGit.pushTags('origin', () => {
+                done();
+              });
             });
           });
         });
       });
-    });
-  }]
+    }, 
+    (versionMetadata, done) => {
+      let gitHub = new GitHubApi(gitHubCredentials);
+      gitHub
+      .getRepo('HopefulLlama', 'llama-rlsr')
+      .createRelease({"tag_name": `v${versionMetadata.newVersion}`}, () => {
+        done();
+      });
+    }
+  ]
 };
